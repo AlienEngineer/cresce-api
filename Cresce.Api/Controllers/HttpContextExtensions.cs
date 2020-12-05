337c5ae854c1f0530c723fd70eb342d763420f09
Sérgio Ferreira <Aimirim.Software@gmail.com>
2020-12-05 19:03:30 +0000

    improved security (#5)
    Co-authored-by: SergioFerreira <sergio.ferreira@ctw.bmwgroup.com>

--- Cresce.Api/Controllers/HttpContextExtensions.cs
+++ Cresce.Api/Controllers/HttpContextExtensions.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using System.Net.Http;
 using Cresce.Core.Authentication;
 using Microsoft.AspNetCore.Http;
 
@@ -8,6 +9,11 @@ namespace Cresce.Api.Controllers
     {
         public static AuthorizedUser GetUser(this HttpRequest request, ITokenFactory tokenFactory)
         {
+            if (!request.Headers["Authorization"].Any())
+            {
+                throw new HttpRequestException();
+            }
+
             var token = request.Headers["Authorization"][0].Split(" ").Skip(1).First();
             return tokenFactory.Decode(token);
         }
